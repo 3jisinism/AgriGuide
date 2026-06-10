604e99700cd1ca9ca8d3c03574ac040fd5952d29
3jisinism <cherry.custodio@evsu.edu.ph>
2026-06-10 10:19:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/agriguide/RecommendationActivity.java
@@ -0,0 +1,57 @@
+package com.example.agriguide;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RecommendationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recommendation);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Back button
+        ImageButton btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(v -> finish());
+
+        // Spinner
+        Spinner locationSpinner;
+        locationSpinner = findViewById(R.id.locationSpinner);
+
+        String[] cities = {
+                "Tacloban City",
+                "Ormoc City",
+                "Baybay City",
+                "Calbayog City"
+        };
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                cities
+        );
+
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item
+        );
+
+        locationSpinner.setAdapter(adapter);
+    }
+}
